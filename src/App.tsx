b8a5708933986d4c1fb75af95642e03771438812
pro <faxridinkf@gmail.com>
2022-04-19 08:50:39 +0500

    init: react pp

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. init
+          Edit <code>src/App.tsx</code> and save to reload. init working tree
         </p>
         <a
           className="App-link"
